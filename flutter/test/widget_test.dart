f74c9f4083cc238f6c5a0357f0e908172db4f199
Tajudeen001-security <gbadamositajudeenwan@gmail.com>
2026-08-29 17:46:18 +0100

    test: update widget test for current app shell

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -1,11 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:jagx_ai/features/auth/auth_service.dart';
 import 'package:jagx_ai/main.dart';
 
 void main() {
   testWidgets('JagX shell loads', (tester) async {
-    final auth = AuthService();
-    await tester.pumpWidget(JagxApp(auth: auth));
+    await tester.pumpWidget(const JagxApp());
+    await tester.pump();
     expect(find.textContaining('JagX'), findsWidgets);
   });
 }
